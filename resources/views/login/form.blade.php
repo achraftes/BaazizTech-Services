f43520a631c57789eb2c40db79e1e7267e365254
achraf <achraf04chke@gmail.com>
2025-05-07 00:42:35 +0100

    update  title  for  page  login

--- resources/views/login/form.blade.php
+++ resources/views/login/form.blade.php
@@ -5,7 +5,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title></title>
+    <title>BAAZIZ-TECHNOLOGIES</title>
     <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
 
     <br> <br> <br>  <br> <br> <br>
