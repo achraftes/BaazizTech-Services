e6655dd128164c3c7e5560d4d3b2d6dde79c58d7
achraf <achraf04chke@gmail.com>
2025-05-06 02:03:31 +0100

    Added Instagram URL to footer icon link

--- resources/views/templete.blade.php
+++ resources/views/templete.blade.php
@@ -443,7 +443,7 @@
                     <a href="#"><i class="fab fa-facebook-f"></i></a>
                     <a href="#"><i class="fab fa-twitter"></i></a>
                     <a href="#"><i class="fab fa-linkedin-in"></i></a>
-                    <a href="#"><i class="fab fa-instagram"></i></a>
+                    <a href="https://www.instagram.com/achraf_chkr/"><i class="fab fa-instagram"></i></a>
                 </div>
             </div>
             
