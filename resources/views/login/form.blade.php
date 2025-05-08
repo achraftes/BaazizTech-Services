2840c8d6dddbc99ce83afc0a81c18e05820305c4
achraf <achraf04chke@gmail.com>
2025-05-08 01:13:34 +0100

    delete br  for  page  login

--- resources/views/login/form.blade.php
+++ resources/views/login/form.blade.php
@@ -8,7 +8,7 @@
     <title>BAAZIZ-TECHNOLOGIES</title>
     <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
 
-    <br> <br> <br>  <br> <br> <br>
+    <br> <br> <br>  <br> <br> 
 </head>
 <body style="padding-top: 56px;">
 <div class="row justify-content-center mt-5">
