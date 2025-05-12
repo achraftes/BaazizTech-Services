72c4c74ca06ff9ea3ae5b7ce1b925ea69bebda8b
achraf <achraf04chke@gmail.com>
2025-05-13 00:04:28 +0100

    delete in section  for  page contact

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -19,9 +19,6 @@
                         <div class="col-md-6 mb-3">
                             <label for="name" class="form-label">Nom complet <span class="text-danger">*</span></label>
                             <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name" value="{{ old('name') }}" required>
-
-                            <label for="name" class="form-label">Nom complet <span class="text-danger">*</span></label>
-                            <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name" value="{{ old('name') }}" required>
                             @error('name')
                                 <div class="invalid-feedback">{{ $message }}</div>
                             @enderror
