0db678bf2e53b105e241bf5248e7adf2bdf0729d
achraf <achraf04chke@gmail.com>
2025-05-17 00:07:24 +0100

    add  new  title  for  page  login

--- resources/views/login/form.blade.php
+++ resources/views/login/form.blade.php
@@ -5,14 +5,14 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>BAAZIZ-TECHNOLOGIE</title>
+    <title>BAAZIZ-TECHNOLOGIES</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
     <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
 </head>
 <body class="bg-light">
     <!-- Main Content -->
-    <div class="container py-5"> <br> <br> <br> <br>   <br>
+    <div class="container py-5"> <br> <br> <br> <br> <br>
         <div class="row justify-content-center">
             <!-- Main Card with Image and Form -->
             <div class="col-md-10">
