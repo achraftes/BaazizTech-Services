face51a694458deddbd925ce2a57c834bf75c16b
achraf <achraf04chke@gmail.com>
2025-05-31 00:26:40 +0100

    create controller Tag

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -7,7 +7,7 @@ namespace App\Http\Controllers;
 use App\Models\Role;
 use Illuminate\Http\Request;
 
-// class RoleController extends Controller
-// {
-//     //
-// }
+class RoleController extends Controller
+{
+    //
+}
--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -7,7 +7,7 @@ namespace App\Http\Controllers;
 use App\Models\Tag;
 use Illuminate\Http\Request;
 
-class TagController extends Controller
-{
-    //
-}
+// class TagController extends Controller
+// {
+//     //
+// }
