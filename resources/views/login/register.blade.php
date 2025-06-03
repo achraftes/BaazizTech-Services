c276870f003b7f21fb67b7578da4eb0a7840b509
achraf <achraf04chke@gmail.com>
2025-06-03 02:07:57 +0100

    make a  function  for  button

--- resources/views/login/register.blade.php
+++ resources/views/login/register.blade.php
@@ -112,6 +112,11 @@
                 password.setAttribute('type', type);
                 this.querySelector('i').className = type === 'password' ? 'fas fa-eye' : 'fas fa-eye-slash';
             });
+             togglePassword.addEventListener('click', function() {
+                const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+                password.setAttribute('type', type);
+                this.querySelector('i').className = type === 'password' ? 'fas fa-eye' : 'fas fa-eye-slash';
+            });
         });
     </script>
 </body>
