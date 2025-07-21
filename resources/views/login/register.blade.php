8dcaf4ceb3b44daa8076026f006bbd24db1a710c
achraf <achraf04chke@gmail.com>
2025-07-21 23:56:14 +0100

    update  stayle  for  page

--- resources/views/login/register.blade.php
+++ resources/views/login/register.blade.php
@@ -154,7 +154,7 @@ Nos consultants
 
 
 <div style="max-width: 1200px; margin: auto;">
-<div style="text-align: center; margin-bottom: 40px;">
+<div style="text-align: center; margin-bottom: 47px;">
 <h2 style="font-size: 38px; font-weight: bold; margin-bottom: 12px;">Obtenez le meilleur du web grâce à notre agence</h2>
 </div>
 <div style="display: grid; grid-template-columns: repeat(auto-fit, minmax(280px, 1fr)); gap: 20px; justify-items: center;">
